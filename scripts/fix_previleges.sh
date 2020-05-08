88a30a8e9d2359aa8aefd66e757953019a5ef8fd
patrot <patrotter@hotmail.com>
2020-05-08 08:30:39 -0500

    Update fix_previleges.sh

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod +x /var/rest-service-app/rest-service-0.0.1-SNAPSHOT.jar
-chmod +x /var/rest-service-app/start_server.sh
-chmod +x /var/rest-service-app/stop_server.sh
+chmod +x /home/ec2-user/rest-service-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/start_server.sh
+chmod +x /home/ec2-user/stop_server.sh
